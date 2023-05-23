9bb34a3ebea23217ef84d443cb8a8600ef00277e
Andrzej Pichliński <andrzej.pichlinski@makolab.com>
2023-05-23 16:43:23 +0200

    Contact.tsx - add sending form

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -12,60 +12,87 @@ import { Form, Link, useActionData, useSearchParams } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return {
-    title: "Login",
+    title: "Contact",
   };
 };
 
 interface ActionData {
+  message?: string;
+  status: {
+    message?: string;
+  }
   errors: {
     email?: string;
-    password?: string;
+    subject?: string;
+    message?: string;
   };
 }
 
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData();
   const email = formData.get("email");
-  const password = formData.get("password");
+  const subject = formData.get("subject");
+  const message = formData.get("message");
   const redirectTo = formData.get("redirectTo");
-  const remember = formData.get("remember");
 
-  if (typeof password !== "string") {
+  if (typeof subject !== "string") {
     return json(
-      { errors: { password: "Valid password is required." } },
+      { errors: { subject: "Valid subject is required." } },
       { status: 400 }
     );
   }
 
-  if (password.length < 6) {
+  if (subject.length < 6) {
     return json(
-      { errors: { password: "Password is too short" } },
+      { errors: { subject: "Subject is too short" } },
       { status: 400 }
     );
   }
+
+  if (message.length < 55) {
+    return json(
+      { errors: { message: "Message is too short" } },
+      { status: 400 }
+    );
+  }
+  
+  return json(
+    { message: "Message has been sent." },
+    { status: 200 }
+  );
 }
 
 export default function Contact() {
   const [searchParams] = useSearchParams();
-  const redirectTo = searchParams.get("redirectTo") ?? "/notes";
+  const redirectTo = searchParams.get("redirectTo") ?? "/";
 
   const actionData = useActionData() as ActionData;
   const emailRef = React.useRef<HTMLInputElement>(null);
-  const passwordRef = React.useRef<HTMLInputElement>(null);
+  const subjectRef = React.useRef<HTMLInputElement>(null);
+  const messageRef = React.useRef<HTMLInputElement>(null);
 
   React.useEffect(() => {
     if (actionData?.errors?.email) {
       emailRef?.current?.focus();
     }
 
-    if (actionData?.errors?.password) {
-      passwordRef?.current?.focus();
+    if (actionData?.errors?.subject) {
+      subjectRef?.current?.focus();
+    }
+
+    if (actionData?.errors?.message) {
+      messageRef?.current?.focus();
     }
   }, [actionData]);
 
   return (
     <div className="flex min-h-full flex-col justify-center">
       <div className="mx-auto w-full max-w-md px-8">
+      {actionData?.message && (
+                <span className="pt-1 text-green-700" id="subject-error">
+                  {actionData?.message}
+                </span>
+              )}
         <Form method="post" className="space-y-6" noValidate>
           <div>
             <label className="text-sm font-medium" htmlFor="email">
@@ -88,37 +115,37 @@ export default function Contact() {
             />
           </div>
           <div>
-            <label className="text-sm font-medium" htmlFor="password">
-              <span className="block text-gray-700">Password</span>
+            <label className="text-sm font-medium" htmlFor="name">
+              <span className="block text-gray-700">Subject</span>
               <span className="block font-light text-gray-700">
                 Must have at least 6 characters.
               </span>
-              {actionData?.errors?.password && (
-                <span className="pt-1 text-red-700" id="password-error">
-                  {actionData?.errors?.password}
+              {actionData?.errors?.subject && (
+                <span className="pt-1 text-red-700" id="subject-error">
+                  {actionData?.errors?.subject}
                 </span>
               )}
             </label>
             <input
-              id="password"
-              type="password"
-              name="password"
+              id="subject"
+              type="subject"
+              name="subject"
               autoComplete=""
               className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
-              aria-invalid={actionData?.errors?.password ? true : undefined}
-              aria-describedby="password-error"
-              ref={passwordRef}
+              aria-invalid={actionData?.errors?.subject ? true : undefined}
+              aria-describedby="subject-error"
+              ref={subjectRef}
             />
           </div>
           <div>
             <label className="text-sm font-medium" htmlFor="message">
               <span className="block text-gray-700">Message</span>
               <span className="block font-light text-gray-700">
-                Must have at least 255 characters.
+                Must have at least 55 characters.
               </span>
-              {actionData?.errors?.password && (
+              {actionData?.errors?.message && (
                 <span className="pt-1 text-red-700" id="message-error">
-                  {actionData?.errors?.password}
+                  {actionData?.errors?.message}
                 </span>
               )}
             </label>
@@ -126,41 +153,17 @@ export default function Contact() {
               id="message"
               name="message"
               autoComplete=""
-              className="w-full rounded border border-gray-500 px-2 py-1 text-lg">
+              className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
+              ref={messageRef}>
               </textarea>
           </div>
           <button
             className="w-full rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
             type="submit"
           >
-            Log in
+            Send
           </button>
           <input type="hidden" name="redirectTo" value={redirectTo} />
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <input
-                className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
-                id="remember"
-                name="remember"
-                type="checkbox"
-              />
-              <label
-                className="ml-2 block text-sm text-gray-900"
-                htmlFor="remember"
-              >
-                Remember me
-              </label>
-            </div>
-            <div className="text-center text-sm text-gray-500">
-              Don't have an account?{" "}
-              <Link
-                className="text-blue-500 underline"
-                to={{ pathname: "/join" }}
-              >
-                Sign up
-              </Link>
-            </div>
-          </div>
         </Form>
       </div>
     </div>
